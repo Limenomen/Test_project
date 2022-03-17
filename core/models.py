1c1cad0e8fbd0fe83b24f5e26ab01feae3e8e2c8
Rsagadatov <rsagadatov@gmail.com>
2022-03-17 15:03:17 +0500

    test

--- core/models.py
+++ core/models.py
@@ -4,14 +4,12 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField('Имя', max_length=128)
 
-    def __str__(self):
-        return self.name
-
 
 class Book(models.Model):
     author = models.ForeignKey('core.Author', on_delete=models.CASCADE, null=True, blank=True, related_name='books')
     name = models.CharField('Название', max_length=128)
     pages = models.IntegerField('Количество страниц', blank=True, null=True)
+    test = models.CharField('test', max_length=12, blank=True)
 
     class Meta:
         ordering = ['pages']
